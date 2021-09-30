22bb9f727d3d8e206e7059386633cb85f1ef83cc
ollielynas <49426014+ollielynas@users.noreply.github.com>
2021-09-30 15:01:58 +1300

    aded moveing grey square

--- script.js
+++ script.js
@@ -17,3 +17,49 @@ var dice = {
     8:"\u2687",
     9:"\u2688",
 }
+
+ele = document.getElementById("bg");
+
+ele.addEventListener('mousedown', function (e) {
+    // Get the target
+    const target = e.target;
+
+    // Get the bounding rectangle of target
+    const rect = target.getBoundingClientRect();
+
+    // Mouse position
+    window.x = e.clientX - rect.left;
+    window.y = e.clientY - rect.top;
+    console.log(window.x,y)
+});
+
+let root = document.documentElement;
+
+root.addEventListener("mousemove", e => {
+    var rect = document.getElementById('bg').getBoundingClientRect(); 
+if (document.body.matches(":active")) {
+  root.style.setProperty('--mouse-x', e.clientX - window.x + "px");
+  root.style.setProperty('--mouse-y', e.clientY - window.y + "px");
+
+  
+  
+}
+});
+
+window.addEventListener( 'load', function() {
+    var box = document.getElementById('bg'),
+        docHeight = document.documentElement.offsetHeight;
+    
+    window.addEventListener( 'scroll', function() {
+          // normalize scroll position as percentage
+      var scrolled = window.scrollY / ( docHeight - window.innerHeight ),
+          transformValue = 'scale('+scrolled+')';
+  
+      box.style.WebkitTransform = transformValue;
+      box.style.MozTransform = transformValue;
+      box.style.OTransform = transformValue;
+      box.style.transform = transformValue;
+      
+    }, false);
+    
+  }, false);
